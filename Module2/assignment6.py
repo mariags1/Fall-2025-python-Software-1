650065ca49eafdc163db38647cb6d78582cbdd5b
maria <maria.siitonen@metropolia.fi>
2025-08-23 10:41:02 +0300

    updates

--- Module2/assignment6.py
+++ Module2/assignment6.py
@@ -1,3 +1,8 @@
+#Write a program that draws two random combinations of numbers for a combination lock:
+#a 3-digit code where each number is between 0 and 9.
+#a 4-digit code where each number is between 1 and 6.
+#Hint: Take a look at the random.randint() function.
+
 import random
 
 number1 = random.randint(0,9)
